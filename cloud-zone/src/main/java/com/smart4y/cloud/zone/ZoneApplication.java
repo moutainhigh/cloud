9a690b128a4f56b9a4336579f27197d304e81738
youtao <youtao@anve.com>
2019-09-05 15:41:07 +0800

    first commit

---
+++ cloud-zone/src/main/java/com/smart4y/cloud/zone/ZoneApplication.java
@@ -0,0 +1,29 @@
+package com.smart4y.cloud.zone;
+
+import com.smart4y.cloud.core.AbstractApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 平台基础服务
+ * 提供系统用户、权限分配、资源、客户端管理
+ *
+ * @author Youtao
+ *         Created by youtao on 2019-09-05.
+ */
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZoneApplication extends AbstractApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        initial(SpringApplication.run(ZoneApplication.class, args));
+    }
+
+    @Override
+    public void run(String... args) {
+    }
+}
---
+++ cloud-zone/src/main/java/com/smart4y/cloud/zone/application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Youtao
+ *         Created by youtao on 2019-09-05.
+ */
+package com.smart4y.cloud.zone.application;
