ce30550fad1980a39799af109378bfe58486ad61
youtao <youtao@anve.com>
2019-10-23 16:22:21 +0800

    release1.0.0

--- pushAliyun.sh
+++ pushAliyun.sh
@@ -4,8 +4,8 @@ TARGET_VERSION=$1
 ALIYUN_REGISTRY=registry.cn-hangzhou.aliyuncs.com
 
 echo "#1 开始编译打包并构建服务镜像"
-echo "mvn clean install package..."
-mvn clean install package
+echo "mvn clean package..."
+mvn clean package
 
 echo "#2 推送服务镜像到阿里云私服"
 docker tag cloud/cloud-hippo:${SOURCE_VERSION} ${ALIYUN_REGISTRY}/smart4y/cloud-hippo:${TARGET_VERSION}
