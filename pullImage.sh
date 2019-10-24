657e022308b45536d514eec484d7e8e4bb240dd6
youtao <youtao@anve.com>
2019-10-24 09:12:05 +0800

    release1.0.0

---
+++ pullImage.sh
@@ -0,0 +1,13 @@
+
+#源镜像版本
+VERSION=$1
+#阿里云私服地址
+ALIYUN_REGISTRY=registry.cn-hangzhou.aliyuncs.com
+
+echo "拉取阿里云私服镜像"
+docker pull ${ALIYUN_REGISTRY}/smart4y/cloud-hippo:${VERSION}
+docker pull ${ALIYUN_REGISTRY}/smart4y/cloud-owl:${VERSION}
+docker pull ${ALIYUN_REGISTRY}/smart4y/cloud-spider:${VERSION}
+
+echo "删除无效镜像"
+docker images|grep none|awk '{print $3}'|xargs docker rmi
--- pushAliyun.sh
+++ pushAliyun.sh
@@ -1,6 +1,9 @@
 
-SOURCE_VERSION=$0
-TARGET_VERSION=$1
+#源镜像版本
+SOURCE_VERSION=$1
+#目标镜像版本
+TARGET_VERSION=$2
+#阿里云私服地址
 ALIYUN_REGISTRY=registry.cn-hangzhou.aliyuncs.com
 
 echo "#1 开始编译打包并构建服务镜像"
@@ -16,3 +19,6 @@ docker push ${ALIYUN_REGISTRY}/smart4y/cloud-owl:${TARGET_VERSION}
 
 docker tag cloud/cloud-spider:${SOURCE_VERSION} ${ALIYUN_REGISTRY}/smart4y/cloud-spider:${TARGET_VERSION}
 docker push ${ALIYUN_REGISTRY}/smart4y/cloud-spider:${TARGET_VERSION}
+
+echo "删除无效镜像"
+docker images|grep none|awk '{print $3}'|xargs docker rmi
